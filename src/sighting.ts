f3ea1b9c1f51e4131b4a8a624a5ff7182303b1f0
Leeewyyy <adam.lewy6@wp.pl>
2022-08-02 11:07:43 +0200

    Added service for sighting downloading and interface class for sighting.

---
+++ src/sighting.ts
@@ -0,0 +1,10 @@
+export interface Sighting {
+    id: number;
+    title: string;
+    foundInCycle: string;
+    priority: string;
+    newIssueStatus: boolean;
+    regressionStatus: string;
+    statusReason: string;
+    component: string;
+}
